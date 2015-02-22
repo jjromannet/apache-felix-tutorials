9d275fbdae6daa47973721986dda29459c43c30d
Jan Roman <mail@jjroman.net>
2015-02-22 14:06:58 +0000

    Implementation of second tutorial described in Felix documentatuib: http://felix.apache.org/documentation/tutorials-examples-and-presentations/apache-felix-osgi-tutorial/apache-felix-tutorial-example-2b.html only difference is that I extracted inner class to separate java file.

--- build.bat
+++ build.bat
@@ -1,2 +1,7 @@
-javac -cp C:\workspaces\libs\felix-framework-4.6.0\bin\felix.jar -d build src\tutorial\example1\Activator.java
+javac -cp %FELIX_HOME%\bin\felix.jar -d build src\tutorial\example1\Activator.java
 jar cfm build\example1.jar src\tutorial\example1\manifest.mf -C build tutorial\example1\Activator.class
+javac -cp %FELIX_HOME%\bin\felix.jar -d build src\tutorial\example2\Activator.java src\tutorial\example2\DictionaryImpl.java src\tutorial\example2\service\DictionaryService.java
+jar cfm build\example2.jar src\tutorial\example2\manifest.mf -C build tutorial\example2\
+
+javac -cp %FELIX_HOME%\bin\felix.jar;build\example2.jar -d build src\tutorial\example2b\Activator.java src\tutorial\example2b\DictionaryImpl.java
+jar cfm build\example2b.jar src\tutorial\example2b\manifest.mf -C build tutorial\example2b\
